501ef9deddd936fde969c502d158eb747ae8d140
MacBook <macbook@MacBook-Pro-User.local>
2023-10-28 21:46:25 +0300

    first test is done

---
+++ TrainProject/src/test/java/api/Specifications.java
@@ -0,0 +1,34 @@
+package api;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+public class Specifications {
+    public static RequestSpecification requestSpecification(String url) {
+        return new RequestSpecBuilder()
+                .setBaseUri(url)
+                .setContentType(ContentType.JSON)
+                .build();
+    }
+
+    public static ResponseSpecification requestSpecificationOK200() {
+        return new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .build();
+    }
+
+    public static ResponseSpecification requestSpecificationError400() {
+        return new ResponseSpecBuilder()
+                .expectStatusCode(400)
+                .build();
+    }
+
+    public static void installSpecification(RequestSpecification requestSpecification, ResponseSpecification responseSpecification) {
+        RestAssured.requestSpecification = requestSpecification;
+        RestAssured.responseSpecification = responseSpecification;
+    }
+}
